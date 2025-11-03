b22ec30059eda992c63543bd680c0abcd9bb20c3
andrei <andrei.bolos123@gmail.com>
2025-11-03 17:44:06 +0200

    add virtual tour borbala farkas

--- src/pages/TurVirtual.jsx
+++ src/pages/TurVirtual.jsx
@@ -1,10 +1,9 @@
+import { useEffect } from 'react';
+
 export default function TurVirtual() {
-  return (
-    <section>
-      <h1 className="page-title">Tur Virtual</h1>
-      <p className="lead">
-        Coming soon... 3.11.2025
-      </p>
-    </section>
-  )
+  useEffect(() => {
+    window.location.href = 'https://borbala-farkas.vercel.app/';
+  }, []);
+
+  return null;
 }
